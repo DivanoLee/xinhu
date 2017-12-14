5c4b25f8d64ee7d8f7fac5bde215c18962a786df
李洋 <530041726@qq.com>
2017-12-14 16:27:58 +0800

    xinhu

---
+++ test44.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+public class x44 extends BaseTest{
+	@Test(priority=0)
+	public void open() throws InterruptedException{
+		webtest.open("http://localhost:8032/xinhu/?m=login");		
+		webtest.type("xpath=//input[@name='adminuser']","admin");
+		webtest.type("xpath=//input[@type='password']","123456");
+		webtest.click("xpath=//button[@name='button']");
+		}
+	@Test(priority=1)
+	public void Test44() throws InterruptedException{
+		webtest.click("xpath=//span[contains(.,'系统')]");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,3,-1)']");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,3,0)']");
+		webtest.click("xpath=//div[@click='kuanshu1']");
+	}
+}
