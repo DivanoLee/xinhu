4bfa9700f9db6b553569836d33a465a6d5e32b4e
李洋 <530041726@qq.com>
2017-12-14 14:49:06 +0800

    xinhu

---
+++ test21.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+public class x21 extends BaseTest{
+	@Test(priority=0)
+	public void open() throws InterruptedException{
+		webtest.open("http://localhost:8032/xinhu/?m=login");		
+		webtest.type("xpath=//input[@name='adminuser']","admin");
+		webtest.type("xpath=//input[@type='password']","123456");
+		webtest.click("xpath=//button[@name='button']");
+		}
+	@Test(priority=1)
+	public void Test21() throws InterruptedException{
+		webtest.click("xpath=//span[contains(.,'系统')]");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,6,-1)']");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,6,3)']");
+		webtest.click("xpath=//div[@click='chush']");
+		webtest.click("xpath=//div[@id='confirm_btn1']");
+	}
+}	
