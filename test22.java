ccd90cbd8a4d5f0dabe0677c682a05a703eaa334
李洋 <530041726@qq.com>
2017-12-14 14:51:57 +0800

    xinhu

---
+++ test22.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+public class x22 extends BaseTest{
+	@Test(priority=0)
+	public void open() throws InterruptedException{
+		webtest.open("http://localhost:8032/xinhu/?m=login");		
+		webtest.type("xpath=//input[@name='adminuser']","admin");
+		webtest.type("xpath=//input[@type='password']","123456");
+		webtest.click("xpath=//button[@name='button']");
+		}
+	@Test(priority=1)
+	public void Test22() throws InterruptedException{
+		webtest.click("xpath=//span[contains(.,'系统')]");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,6,-1)']");
+		webtest.click("xpath=//div[@onclick='clickmenu(this,6,3)']");
+		webtest.click("xpath=//div[@click='deluser']");
+		webtest.click("xpath=//div[@id='confirm_btn1']");
+	}
+}	
